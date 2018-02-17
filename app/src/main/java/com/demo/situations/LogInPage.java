bac4a9b112539a21f9ebf7fa20187870716953e5
PP <situationsdemo@gmail.com>
2018-02-16 22:12:14 -0500

    Added google sign in option

--- app/src/main/java/com/demo/situations/LogInPage.java
+++ app/src/main/java/com/demo/situations/LogInPage.java
@@ -11,37 +11,52 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LogInPage extends AppCompatActivity implements View.OnClickListener {
 
     EditText editTextemail, editTextpassword;
-    Button signinbutton, signupbutton;
+    Button signinbutton, signupbutton, googlesigninButton;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
     private Intent main_activity;
+    GoogleSignInOptions gso;
+    final private int RC_SIGN_IN = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
         mAuth = FirebaseAuth.getInstance();
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
         editTextemail = findViewById(R.id.editTextemail);
         editTextpassword = findViewById(R.id.editTextpassword);
 
         signinbutton = findViewById(R.id.signinbutton);
         signupbutton = findViewById(R.id.signupbutton);
-
+        googlesigninButton = findViewById(R.id.googlesigninButton);
         signinbutton.setOnClickListener(this);
         signupbutton.setOnClickListener(this);
+        googlesigninButton.setOnClickListener(this);
 
         main_activity = new Intent(this,MainActivity.class);
     }
@@ -62,7 +77,9 @@ public class LogInPage extends AppCompatActivity implements View.OnClickListener
             case R.id.signinbutton:
                 SignIn();
                 break;
-
+            case R.id.googlesigninButton:
+                SigninWithGoogle();
+                break;
             case R.id.signupbutton:
                 CreateNewUser();
                 break;
@@ -87,29 +104,34 @@ public class LogInPage extends AppCompatActivity implements View.OnClickListener
                             startActivity(main_activity);
                         }
                         else{
-                            try{
-                                throw task.getException();
-                            }
-                            catch (FirebaseNetworkException e){
-                                ShowAlert("No Network!");
-                            }
-                            catch (FirebaseAuthInvalidUserException e){
-                                ShowAlert("Account with this email doesn't exist!");
-                            }
-                            catch (FirebaseAuthInvalidCredentialsException e){
-                                ShowAlert("the password is invalid");
-                            }
-                            catch (FirebaseException e){
-                                ShowAlert(e.getMessage());
-                            }
-                            catch (Exception e) {
-                                ShowAlert(e.getMessage());
-                            }
+                            ThrowException(task.getException());
                         }
                     }
                 });
     }
 
+    private void SigninWithGoogle(){
+
+        Intent _intent = GoogleSignIn.getClient(this,gso).getSignInIntent();
+        startActivityForResult(_intent,RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                FirebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                ThrowException(task.getException());
+            }
+        }
+    }
     private void CreateNewUser() {
         String email = editTextemail.getText().toString().trim();
         String password = editTextpassword.getText().toString().trim();
@@ -130,24 +152,29 @@ public class LogInPage extends AppCompatActivity implements View.OnClickListener
                                 startActivity(main_activity);
                         }
                         else{
-                            try{
-                                throw task.getException();
-                            }
-                            catch (FirebaseAuthUserCollisionException e){
-                                ShowAlert("Email already in use.");
-                            }
-                            catch (FirebaseNetworkException e){
-                                ShowAlert("No network");
-                            }
-                            catch (Exception e) {
-                                ShowAlert(e.getMessage());
-                            }
+                                ThrowException(task.getException());
                         }
                     }
                 });
 
     }
 
+    private void FirebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            currentUser = mAuth.getCurrentUser();
+                            startActivity(main_activity);
+                        } else {
+                            ThrowException(task.getException());
+                        }
+                    }
+                });
+    }
     private void ShowAlert(String msg){
         AlertDialog.Builder myAlert = new AlertDialog.Builder(this)
                 .setMessage(msg)
@@ -159,5 +186,29 @@ public class LogInPage extends AppCompatActivity implements View.OnClickListener
                 });
         myAlert.create();
         myAlert.show();
+    }
+
+    private void ThrowException(Exception exception){
+        try{
+            throw exception;
+        }
+        catch (FirebaseNetworkException e){
+            ShowAlert("No Network!");
+        }
+        catch (FirebaseAuthInvalidUserException e){
+            ShowAlert("Account with this email doesn't exist!");
+        }
+        catch (FirebaseAuthInvalidCredentialsException e){
+            ShowAlert("the password is invalid");
+        }
+        catch (FirebaseAuthUserCollisionException e){
+            ShowAlert("Email already in use.");
+        }
+        catch (FirebaseException e){
+            ShowAlert(e.getMessage());
+        }
+        catch (Exception e){
+
+        }
     }
 }
